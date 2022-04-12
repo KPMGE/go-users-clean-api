adc242c846d87bfaf9094e8e722c8704d9961623
KPMGE <jesuskevin254@gmail.com>
2022-04-12 17:28:54 -0300

    :white_check_mark: ensure create account returns error if invalid email is provided

--- tests/domain/entities/account_test.go
+++ tests/domain/entities/account_test.go
@@ -1,8 +1,11 @@
 package entities_test
 
 import (
-	"github.com/stretchr/testify/require"
+	"errors"
 	"testing"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/stretchr/testify/require"
 )
 
 type Account struct {
@@ -17,11 +20,24 @@ const (
 	fakeAccountPassword string = "any_password"
 )
 
+func (account *Account) isValid() error {
+	validEmail := govalidator.IsEmail(account.Email)
+	if !validEmail {
+		return errors.New("Invalid email!")
+	}
+	return nil
+}
+
 func NewAccount(userName string, email string, password string) (*Account, error) {
 	account := Account{
 		Email:    email,
 		UserName: userName,
 		Password: password,
+	}
+
+	err := account.isValid()
+	if err != nil {
+		return nil, err
 	}
 
 	return &account, nil
@@ -34,4 +50,12 @@ func TestNewAccount_WithRightData(t *testing.T) {
 	require.Equal(t, account.Email, fakeAccountEmail)
 	require.Equal(t, account.UserName, fakeAccountUserName)
 	require.Equal(t, account.Password, fakeAccountPassword)
+}
+
+func TestNewAccount_WithInvalidEmail(t *testing.T) {
+	account, err := NewAccount(fakeAccountUserName, "any_invalid_email", fakeAccountPassword)
+
+	require.Nil(t, account)
+	require.Error(t, err)
+	require.Equal(t, err.Error(), "Invalid email!")
 }

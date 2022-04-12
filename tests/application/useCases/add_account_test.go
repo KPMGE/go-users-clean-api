0a559fce4f96a98f6fe05e2a84de9d89ee8c91c8
KPMGE <jesuskevin254@gmail.com>
2022-04-12 18:36:07 -0300

    :white_check_mark: ensure AddAccount creates account if right data is provided

---
+++ tests/application/useCases/add_account_test.go
@@ -0,0 +1,89 @@
+package usecases_test
+
+import (
+	"github.com/KPMGE/go-users-clean-api/src/domain/entities"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+type AccountRepository interface {
+	checkAccountByEmail(email string) bool
+	save(accout *entities.Account) error
+}
+
+type Hasher interface {
+	hash(plainText string) string
+}
+
+type AddAccountUseCase struct {
+	accountRepository AccountRepository
+	hasher            Hasher
+}
+
+type AddAccountOutputDTO struct {
+	ID       string
+	UserName string
+	Email    string
+}
+
+func (useCase *AddAccountUseCase) addAccount(userName string, email string, password string, confirmPassword string) (*AddAccountOutputDTO, error) {
+	account, _ := entities.NewAccount(userName, email, password)
+	useCase.hasher.hash(password)
+	output := AddAccountOutputDTO{
+		ID:       account.ID,
+		UserName: account.UserName,
+		Email:    account.Email,
+	}
+	return &output, nil
+}
+
+func NewAddAccountUseCase(accountRepository AccountRepository, hasher Hasher) *AddAccountUseCase {
+	return &AddAccountUseCase{
+		accountRepository: accountRepository,
+		hasher:            hasher,
+	}
+}
+
+type hasherSpy struct {
+	input string
+}
+
+func (hasher *hasherSpy) hash(plainText string) string {
+	hasher.input = plainText
+	return "hashed_plain_text"
+}
+
+type FakeAccountRepository struct{}
+
+func (repo *FakeAccountRepository) checkAccountByEmail(email string) bool {
+	return true
+}
+
+func (repo *FakeAccountRepository) save(accout *entities.Account) error {
+	return nil
+}
+
+func NewHasherSpy() *hasherSpy {
+	return &hasherSpy{}
+}
+
+func NewFakeAccountRepository() *FakeAccountRepository {
+	return &FakeAccountRepository{}
+}
+
+const fakeUserName string = "any_user_name"
+const fakeEmail string = "any_valid_email@gmail.com"
+const fakePassword string = "any_password"
+
+func TestAddAccountUseCase_WithRightData(t *testing.T) {
+	repo := NewFakeAccountRepository()
+	hasher := NewHasherSpy()
+	sut := NewAddAccountUseCase(repo, hasher)
+
+	createdAccount, err := sut.addAccount(fakeUserName, fakeEmail, fakePassword, fakePassword)
+
+	require.Nil(t, err)
+	require.Equal(t, hasher.input, fakePassword)
+	require.Equal(t, createdAccount.Email, fakeEmail)
+	require.Equal(t, createdAccount.UserName, fakeUserName)
+}

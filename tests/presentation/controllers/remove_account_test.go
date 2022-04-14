4685f4eaf541b8ff63e81b4b29f88aea5171542d
KPMGE <jesuskevin254@gmail.com>
2022-04-13 22:43:24 -0300

    :white_check_mark: ensure RemoveAccountController badRequest if wrong id is provided

--- tests/presentation/controllers/remove_account_test.go
+++ tests/presentation/controllers/remove_account_test.go
@@ -1,12 +1,14 @@
 package controllers_test
 
 import (
+	"testing"
+
 	usecases "github.com/KPMGE/go-users-clean-api/src/application/useCases"
+	"github.com/KPMGE/go-users-clean-api/src/domain/entities"
 	"github.com/KPMGE/go-users-clean-api/src/presentation/helpers"
 	"github.com/KPMGE/go-users-clean-api/src/presentation/protocols"
 	mocks_test "github.com/KPMGE/go-users-clean-api/tests/application/mocks"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 type RemoveAccountController struct {
@@ -14,7 +16,11 @@ type RemoveAccountController struct {
 }
 
 func (controller *RemoveAccountController) Handle(accountId string) *protocols.HttpResponse {
-	return helpers.Ok("account deleted")
+	message, err := controller.useCase.Remove(accountId)
+	if err != nil {
+		return helpers.BadRequest(err)
+	}
+	return helpers.Ok(message)
 }
 
 func NewRemoveAccountController(useCase *usecases.RemoveAccountUseCase) *RemoveAccountController {
@@ -25,6 +31,8 @@ func NewRemoveAccountController(useCase *usecases.RemoveAccountUseCase) *RemoveA
 
 func TestRemoveAccountController_WithCorrectID(t *testing.T) {
 	repo := mocks_test.NewFakeAccountRepository()
+	fakeAccount, _ := entities.NewAccount("any_username", "any_valid_email@gmail.com", "any_pass")
+	repo.FindAccountByIdOutput = fakeAccount
 	useCase := usecases.NewRemoveAccountUseCase(repo)
 	sut := NewRemoveAccountController(useCase)
 
@@ -32,4 +40,16 @@ func TestRemoveAccountController_WithCorrectID(t *testing.T) {
 
 	require.Equal(t, httpResponse.StatusCode, 200)
 	require.Equal(t, httpResponse.Body, "account deleted")
+}
+
+func TestRemoveAccountController_WithWrongID(t *testing.T) {
+	repo := mocks_test.NewFakeAccountRepository()
+	repo.FindAccountByIdOutput = nil
+	useCase := usecases.NewRemoveAccountUseCase(repo)
+	sut := NewRemoveAccountController(useCase)
+
+	httpResponse := sut.Handle("any_invalid_id")
+
+	require.Equal(t, httpResponse.StatusCode, 400)
+	require.Equal(t, httpResponse.Body, "there is no account with this id")
 }

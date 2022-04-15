c8b1cdb799ab7b6ed8cb40b1b2b7175fa8d5ba1a
KPMGE <jesuskevin254@gmail.com>
2022-04-15 11:33:28 -0300

    :recycle: move ListUsersController to its src folder

--- tests/presentation/controllers/list_users_test.go
+++ tests/presentation/controllers/list_users_test.go
@@ -7,37 +7,17 @@ import (
 	dto "github.com/KPMGE/go-users-clean-api/src/application/DTO"
 	usecases "github.com/KPMGE/go-users-clean-api/src/application/useCases"
 	"github.com/KPMGE/go-users-clean-api/src/domain/entities"
-	"github.com/KPMGE/go-users-clean-api/src/presentation/helpers"
+	"github.com/KPMGE/go-users-clean-api/src/presentation/controllers"
 	"github.com/KPMGE/go-users-clean-api/src/presentation/protocols"
 	mocks_test "github.com/KPMGE/go-users-clean-api/tests/application/mocks"
 	"github.com/stretchr/testify/require"
 )
 
-type ListUsersController struct {
-	useCase *usecases.ListUsersUseCase
-}
-
-func NewListUsersController(useCase *usecases.ListUsersUseCase) *ListUsersController {
-	return &ListUsersController{
-		useCase: useCase,
-	}
-}
-
-func (controller *ListUsersController) Handle(request *protocols.HttpRequest) *protocols.HttpResponse {
-	users := controller.useCase.List()
-	jsonUsers, err := json.Marshal(users)
-
-	if err != nil {
-		return helpers.ServerError(err)
-	}
-	return helpers.Ok(jsonUsers)
-}
-
-func MakeListUsersSut() (*ListUsersController, *mocks_test.UserRepositorySpy) {
+func MakeListUsersSut() (*controllers.ListUsersController, *mocks_test.UserRepositorySpy) {
 	repo := mocks_test.NewUserRepositorySpy()
 	repo.ListUsersOutput = []*entities.User{}
 	useCase := usecases.NewListUsersUseCase(repo)
-	sut := NewListUsersController(useCase)
+	sut := controllers.NewListUsersController(useCase)
 	return sut, repo
 }
 

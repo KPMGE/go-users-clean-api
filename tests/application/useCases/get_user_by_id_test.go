7432e05b66975c1c4f7303b23e44f194129b1131
KPMGE <jesuskevin254@gmail.com>
2022-04-17 18:39:29 -0300

    :white_check_mark: ensure GetUserByIdUseCase returns right output on success

--- tests/application/useCases/get_user_by_id_test.go
+++ tests/application/useCases/get_user_by_id_test.go
@@ -1,12 +1,16 @@
 package usecases_test
 
 import (
+	"log"
 	"testing"
 
 	"github.com/KPMGE/go-users-clean-api/src/application/protocols"
+	"github.com/KPMGE/go-users-clean-api/src/domain/entities"
 	mocks_test "github.com/KPMGE/go-users-clean-api/tests/application/mocks"
 	"github.com/stretchr/testify/require"
 )
+
+const fakeName string = "any_user_name"
 
 type GetUserByIdUseCaseOutputDTO struct {
 	ID       string
@@ -25,12 +29,23 @@ func NewGetUserByIdUseCase(repo protocols.UserRepository) *GetUserByIdUseCase {
 	}
 }
 
-func (useCase *GetUserByIdUseCase) Get(userId string) {
-	useCase.userRepository.GetById(userId)
+func (useCase *GetUserByIdUseCase) Get(userId string) *GetUserByIdUseCaseOutputDTO {
+	foundUser, _ := useCase.userRepository.GetById(userId)
+	return &GetUserByIdUseCaseOutputDTO{
+		ID:       foundUser.ID,
+		Name:     foundUser.Name,
+		UserName: foundUser.UserName,
+		Email:    foundUser.Email,
+	}
 }
 
 func MakeGetUserByIdSut() (*GetUserByIdUseCase, *mocks_test.UserRepositorySpy) {
 	repo := mocks_test.NewUserRepositorySpy()
+	fakeUser, err := entities.NewUser(fakeName, fakeUserName, fakeEmail)
+	if err != nil {
+		log.Fatal(err)
+	}
+	repo.GetByidOutput = fakeUser
 	sut := NewGetUserByIdUseCase(repo)
 	return sut, repo
 }
@@ -39,4 +54,13 @@ func TestGetUserByIdUseCase_ShouldCallRepositoryCorrectly(t *testing.T) {
 	sut, repo := MakeGetUserByIdSut()
 	sut.Get(FAKE_USER_ID)
 	require.Equal(t, FAKE_USER_ID, repo.GetByidInput)
+}
+
+func TestGetUserByIdUseCase_ShouldReturnFoundUser(t *testing.T) {
+	sut, _ := MakeGetUserByIdSut()
+	foundUser := sut.Get(FAKE_USER_ID)
+	require.Equal(t, fakeName, foundUser.Name)
+	require.Equal(t, fakeUserName, foundUser.UserName)
+	require.Equal(t, fakeEmail, foundUser.Email)
+	require.NotNil(t, foundUser.ID)
 }

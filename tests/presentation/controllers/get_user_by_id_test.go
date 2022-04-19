1dbe3d01466d2c163230f37fe367e6e24d8dfebc
KPMGE <jesuskevin254@gmail.com>
2022-04-18 22:44:03 -0300

    :white_check_mark: ensure GetUserByIdController calls useCase with right data

---
+++ tests/presentation/controllers/get_user_by_id_test.go
@@ -0,0 +1,37 @@
+package controllers_test
+
+import (
+	usecases "github.com/KPMGE/go-users-clean-api/src/application/useCases"
+	"github.com/KPMGE/go-users-clean-api/src/presentation/protocols"
+	mocks_test "github.com/KPMGE/go-users-clean-api/tests/application/mocks"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+const FAKE_USER_ID string = "any_user_id"
+
+type GetUserByIdController struct {
+	useCase *usecases.GetUserByIdUseCase
+}
+
+func NewGetUserByIdController(useCase *usecases.GetUserByIdUseCase) *GetUserByIdController {
+	return &GetUserByIdController{
+		useCase: useCase,
+	}
+}
+
+func (controller *GetUserByIdController) Handle(request *protocols.HttpRequest) *protocols.HttpResponse {
+	controller.useCase.Get(string(request.Params))
+	return nil
+}
+
+func TestGetUserByIdController_ShouldCallUseCaseWithRightData(t *testing.T) {
+	repo := mocks_test.NewUserRepositorySpy()
+	useCase := usecases.NewGetUserByIdUseCase(repo)
+	sut := NewGetUserByIdController(useCase)
+	fakeRequest := protocols.NewHtppRequest(nil, []byte(FAKE_USER_ID))
+
+	sut.Handle(fakeRequest)
+
+	require.Equal(t, FAKE_USER_ID, repo.GetByidInput)
+}

3ff9e8f45ecf5f33e001a28088dd52e366c3a6e7
KPMGE <jesuskevin254@gmail.com>
2022-04-21 19:42:58 -0300

    :white_check_mark: ensure AddBookUseCase returns error if no user is found

--- tests/application/useCases/add_gift_test.go
+++ tests/application/useCases/add_gift_test.go
@@ -1,9 +1,12 @@
 package usecases_test
 
 import (
+	"errors"
 	"testing"
 
+	"github.com/KPMGE/go-users-clean-api/src/application/protocols"
 	"github.com/KPMGE/go-users-clean-api/src/domain/entities"
+	mocks_test "github.com/KPMGE/go-users-clean-api/tests/application/mocks"
 	"github.com/stretchr/testify/require"
 )
 
@@ -52,38 +55,59 @@ func NewBookRepositorySpy() *AddBookRepositorySpy {
 }
 
 type AddBookUseCase struct {
-	repo AddBookRepository
+	bookRepo AddBookRepository
+	userRepo protocols.UserRepository
 }
 
-func NewAddBookUseCase(repo AddBookRepository) *AddBookUseCase {
+func NewAddBookUseCase(bookRepo AddBookRepository, userRepo protocols.UserRepository) *AddBookUseCase {
 	return &AddBookUseCase{
-		repo: repo,
+		bookRepo: bookRepo,
+		userRepo: userRepo,
 	}
 }
 
 func (useCase *AddBookUseCase) Add(input *AddBookUseCaseInputDTO) (*AddBookUseCaseOutputDTO, error) {
-	user, _ := entities.NewUser("any_name", "any_username", "any_valid_email@gmail.com")
-	newBook, _ := entities.NewBook(input.Title, input.Author, input.Description, input.Price, user)
-	useCase.repo.Add(newBook)
+	foundUser, _ := useCase.userRepo.GetById(input.UserId)
+	if foundUser == nil {
+		return nil, errors.New("User not found!")
+	}
+
+	newBook, _ := entities.NewBook(input.Title, input.Author, input.Description, input.Price, foundUser)
+	useCase.bookRepo.Add(newBook)
 	return nil, nil
 }
 
-func MakeAddBookSut() (*AddBookUseCase, *AddBookRepositorySpy) {
-	repo := NewBookRepositorySpy()
-	sut := NewAddBookUseCase(repo)
-	return sut, repo
+func MakeAddBookSut() (*AddBookUseCase, *AddBookRepositorySpy, *mocks_test.UserRepositorySpy) {
+	bookRepo := NewBookRepositorySpy()
+	userRepo := mocks_test.NewUserRepositorySpy()
+	sut := NewAddBookUseCase(bookRepo, userRepo)
+	return sut, bookRepo, userRepo
 }
 
 func TestAddBookUseCase_ShouldCallRepositoryWithRightData(t *testing.T) {
-	sut, repo := MakeAddBookSut()
+	sut, bookRepo, userRepo := MakeAddBookSut()
+	fakeUser, _ := entities.NewUser("any_name", "any_username", "any_email@gmail.com")
+	userRepo.GetByidOutput = fakeUser
 	fakeInput := NewAddBookUseCaseInputDTO("any_title", "any_author", 342.2, "any_description", "any_valid_user_id")
 
 	sut.Add(fakeInput)
 
-	require.Equal(t, fakeInput.Author, repo.input.Author)
-	require.Equal(t, fakeInput.Description, repo.input.Description)
-	require.Equal(t, fakeInput.Price, repo.input.Price)
-	require.Equal(t, fakeInput.Title, repo.input.Title)
-	require.NotNil(t, repo.input.ID)
-	require.NotNil(t, repo.input.User)
+	require.Equal(t, fakeInput.Author, bookRepo.input.Author)
+	require.Equal(t, fakeInput.Description, bookRepo.input.Description)
+	require.Equal(t, fakeInput.Price, bookRepo.input.Price)
+	require.Equal(t, fakeInput.Title, bookRepo.input.Title)
+	require.NotNil(t, bookRepo.input.ID)
+	require.NotNil(t, bookRepo.input.User)
+}
+
+func TestAddBookUseCase_ShouldReturnErrorIfWrongUserIdIsGiven(t *testing.T) {
+	sut, _, userRepo := MakeAddBookSut()
+	userRepo.GetByidOutput = nil
+	fakeInput := NewAddBookUseCaseInputDTO("any_title", "any_author", 342.2, "any_description", "any_invalid_user_id")
+
+	output, err := sut.Add(fakeInput)
+
+	require.Nil(t, output)
+	require.Error(t, err)
+	require.Equal(t, "User not found!", err.Error())
 }

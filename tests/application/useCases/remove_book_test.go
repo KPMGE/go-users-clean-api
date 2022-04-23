0622c0f5fa19da8ea668efb7fd914ba76eb05990
KPMGE <jesuskevin254@gmail.com>
2022-04-23 11:34:12 -0300

    :white_check_mark: ensure RemoveBookUseCase calls FindBookRepository with right bookId

--- tests/application/useCases/remove_book_test.go
+++ tests/application/useCases/remove_book_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/KPMGE/go-users-clean-api/src/domain/entities"
 	"github.com/stretchr/testify/require"
 )
 
@@ -11,9 +12,18 @@ type RemoveBookRepository interface {
 	Remove(bookId string) error
 }
 
+type FindBookRepository interface {
+	Find(bookId string) (*entities.Book, error)
+}
+
 type RemoveBookRepositorySpy struct {
 	RemoveInput string
 	RemoveError error
+}
+
+type FindBookRepositorySpy struct {
+	FindInput string
+	FindError error
 }
 
 func (repo *RemoveBookRepositorySpy) Remove(bookId string) error {
@@ -25,45 +35,67 @@ func NewRemoveBookRepositorySpy() *RemoveBookRepositorySpy {
 	return &RemoveBookRepositorySpy{}
 }
 
-type RemoveBookUseCase struct {
-	bookRepo RemoveBookRepository
+func (repo *FindBookRepositorySpy) Find(bookId string) (*entities.Book, error) {
+	repo.FindInput = bookId
+	return nil, repo.FindError
 }
 
-func NewRemoveBookUseCase(repo RemoveBookRepository) *RemoveBookUseCase {
+func NewFindBookRepositorySpy() *FindBookRepositorySpy {
+	return &FindBookRepositorySpy{}
+}
+
+type RemoveBookUseCase struct {
+	removeBookRepo RemoveBookRepository
+	findBookRepo   FindBookRepository
+}
+
+func NewRemoveBookUseCase(removeBookRepo RemoveBookRepository, findBookRepo FindBookRepository) *RemoveBookUseCase {
 	return &RemoveBookUseCase{
-		bookRepo: repo,
+		removeBookRepo: removeBookRepo,
+		findBookRepo:   findBookRepo,
 	}
 }
 
 func (useCase *RemoveBookUseCase) Remove(bookId string) error {
-	err := useCase.bookRepo.Remove(bookId)
+	useCase.findBookRepo.Find(bookId)
+	err := useCase.removeBookRepo.Remove(bookId)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func MakeRemoveBookSut() (*RemoveBookUseCase, *RemoveBookRepositorySpy) {
-	bookRepo := NewRemoveBookRepositorySpy()
-	bookRepo.RemoveError = nil
-	sut := NewRemoveBookUseCase(bookRepo)
-	return sut, bookRepo
+func MakeRemoveBookSut() (*RemoveBookUseCase, *RemoveBookRepositorySpy, *FindBookRepositorySpy) {
+	removeBookRepo := NewRemoveBookRepositorySpy()
+	removeBookRepo.RemoveError = nil
+	findBookRepo := NewFindBookRepositorySpy()
+	findBookRepo.FindError = nil
+	sut := NewRemoveBookUseCase(removeBookRepo, findBookRepo)
+	return sut, removeBookRepo, findBookRepo
 }
 
 func TestRemoveBookUseCase_ShouldCallRepositoryWithRightBookId(t *testing.T) {
-	sut, bookRepo := MakeRemoveBookSut()
+	sut, removeBookRepo, _ := MakeRemoveBookSut()
 
 	sut.Remove("any_valid_book_id")
 
-	require.Equal(t, "any_valid_book_id", bookRepo.RemoveInput)
+	require.Equal(t, "any_valid_book_id", removeBookRepo.RemoveInput)
 }
 
 func TestRemoveBookUseCase_ShouldReturnErrorIfRepositoryReturnsError(t *testing.T) {
-	sut, bookRepo := MakeRemoveBookSut()
-	bookRepo.RemoveError = errors.New("repo error")
+	sut, removeBookRepo, _ := MakeRemoveBookSut()
+	removeBookRepo.RemoveError = errors.New("repo error")
 
 	err := sut.Remove("any_invalid_id")
 
 	require.Error(t, err)
 	require.Equal(t, "repo error", err.Error())
+}
+
+func TestRemoveBookUseCase_ShouldCallFindBookRepositoryWithRightBookId(t *testing.T) {
+	sut, _, findBookRepo := MakeRemoveBookSut()
+
+	sut.Remove("any_book_id")
+
+	require.Equal(t, "any_book_id", findBookRepo.FindInput)
 }

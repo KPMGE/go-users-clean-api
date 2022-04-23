f04576b4daa94a93d7356e83ae62dabee834b317
KPMGE <jesuskevin254@gmail.com>
2022-04-23 11:23:59 -0300

    :white_check_mark: ensure RemoveBookUseCase returns error if repository returns error

--- tests/application/useCases/remove_book_test.go
+++ tests/application/useCases/remove_book_test.go
@@ -1,6 +1,7 @@
 package usecases_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -12,11 +13,12 @@ type RemoveBookRepository interface {
 
 type RemoveBookRepositorySpy struct {
 	RemoveInput string
+	RemoveError error
 }
 
 func (repo *RemoveBookRepositorySpy) Remove(bookId string) error {
 	repo.RemoveInput = bookId
-	return nil
+	return repo.RemoveError
 }
 
 func NewRemoveBookRepositorySpy() *RemoveBookRepositorySpy {
@@ -33,12 +35,17 @@ func NewRemoveBookUseCase(repo RemoveBookRepository) *RemoveBookUseCase {
 	}
 }
 
-func (useCase *RemoveBookUseCase) Remove(bookId string) {
-	useCase.bookRepo.Remove(bookId)
+func (useCase *RemoveBookUseCase) Remove(bookId string) error {
+	err := useCase.bookRepo.Remove(bookId)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func MakeRemoveBookSut() (*RemoveBookUseCase, *RemoveBookRepositorySpy) {
 	bookRepo := NewRemoveBookRepositorySpy()
+	bookRepo.RemoveError = nil
 	sut := NewRemoveBookUseCase(bookRepo)
 	return sut, bookRepo
 }
@@ -49,4 +56,14 @@ func TestRemoveBookUseCase_ShouldCallRepositoryWithRightBookId(t *testing.T) {
 	sut.Remove("any_valid_book_id")
 
 	require.Equal(t, "any_valid_book_id", bookRepo.RemoveInput)
+}
+
+func TestRemoveBookUseCase_ShouldReturnErrorIfRepositoryReturnsError(t *testing.T) {
+	sut, bookRepo := MakeRemoveBookSut()
+	bookRepo.RemoveError = errors.New("repo error")
+
+	err := sut.Remove("any_invalid_id")
+
+	require.Error(t, err)
+	require.Equal(t, "repo error", err.Error())
 }

afe37bacde2fb11b07673afed3a87b4ddef14cb4
KPMGE <jesuskevin254@gmail.com>
2022-04-23 11:19:25 -0300

    :recycle: move sut creation to factory helper method

--- tests/application/useCases/remove_book_test.go
+++ tests/application/useCases/remove_book_test.go
@@ -37,9 +37,14 @@ func (useCase *RemoveBookUseCase) Remove(bookId string) {
 	useCase.bookRepo.Remove(bookId)
 }
 
-func TestRemoveBookUseCase_ShouldCallRepositoryWithRightBookId(t *testing.T) {
+func MakeRemoveBookSut() (*RemoveBookUseCase, *RemoveBookRepositorySpy) {
 	bookRepo := NewRemoveBookRepositorySpy()
 	sut := NewRemoveBookUseCase(bookRepo)
+	return sut, bookRepo
+}
+
+func TestRemoveBookUseCase_ShouldCallRepositoryWithRightBookId(t *testing.T) {
+	sut, bookRepo := MakeRemoveBookSut()
 
 	sut.Remove("any_valid_book_id")
 

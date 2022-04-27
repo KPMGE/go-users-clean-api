dbb9cda07301bd3807726eeaafdcd4b59f39f8fe
KPMGE <jesuskevin254@gmail.com>
2022-04-27 12:27:39 -0300

    :white_check_mark: ensure GetBookByIdUseCase returns right data on success

--- tests/application/useCases/get_book_by_id_test.go
+++ tests/application/useCases/get_book_by_id_test.go
@@ -41,6 +41,8 @@ func NewGetBookByIdUseCase(getBookRepo GetBookRepository) *GetBookByIdUseCase {
 
 func MakeGetBookByIdSut() (*GetBookByIdUseCase, *GetBookByIdRepositorySpy) {
 	getBookRepo := NewGetBookByIdRepositorySpy()
+	fakeBook, _ := entities.NewBook("any_title", "any_author", "any_description", 100.23, "any_user_id")
+	getBookRepo.output = fakeBook
 	sut := NewGetBookByIdUseCase(getBookRepo)
 	return sut, getBookRepo
 }
@@ -56,4 +58,17 @@ func TestGetBookByIdUseCase_ShouldRetunNilIfNoBookIsFound(t *testing.T) {
 	getBookRepo.output = nil
 	foundBook := sut.GetById("any_book_id")
 	require.Nil(t, foundBook)
+}
+
+func TestGetBookByIdUseCase_ShouldRetunRightBookOnSuccess(t *testing.T) {
+	sut, _ := MakeGetBookByIdSut()
+	foundBook := sut.GetById("any_book_id")
+	require.Equal(t, "any_title", foundBook.Title)
+	require.Equal(t, "any_author", foundBook.Author)
+	require.Equal(t, "any_user_id", foundBook.UserId)
+	require.Equal(t, "any_description", foundBook.Description)
+	require.Equal(t, 100.23, foundBook.Price)
+	require.NotNil(t, foundBook.CreatedAt)
+	require.NotNil(t, foundBook.UpdatedAt)
+	require.NotNil(t, foundBook.ID)
 }

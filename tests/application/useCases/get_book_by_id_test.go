3fc91c4a97026dc53435eae3f0a6541c19766162
KPMGE <jesuskevin254@gmail.com>
2022-04-27 12:13:09 -0300

    :white_check_mark: ensure GetBookByIdUseCase calls repository with right data

---
+++ tests/application/useCases/get_book_by_id_test.go
@@ -0,0 +1,48 @@
+package usecases_test
+
+import (
+	"testing"
+
+	"github.com/KPMGE/go-users-clean-api/src/domain/entities"
+	"github.com/stretchr/testify/require"
+)
+
+type GetBookByIdRepositoryMock struct {
+	input string
+}
+
+func (repo *GetBookByIdRepositoryMock) Get(bookId string) *entities.Book {
+	repo.input = bookId
+	return nil
+}
+
+func NewGetBookByIdRepositoryMock() *GetBookByIdRepositoryMock {
+	return &GetBookByIdRepositoryMock{}
+}
+
+type GetBookRepository interface {
+	Get(bookId string) *entities.Book
+}
+
+type GetBookByIdUseCase struct {
+	getBookRepo GetBookRepository
+}
+
+func (useCase *GetBookByIdUseCase) GetById(bookId string) {
+	useCase.getBookRepo.Get(bookId)
+}
+
+func NewGetBookByIdUseCase(getBookRepo GetBookRepository) *GetBookByIdUseCase {
+	return &GetBookByIdUseCase{
+		getBookRepo: getBookRepo,
+	}
+}
+
+func TestGetBookByIdUseCase_ShouldCallRepositoryWithRightData(t *testing.T) {
+	getBookRepo := NewGetBookByIdRepositoryMock()
+	sut := NewGetBookByIdUseCase(getBookRepo)
+
+	sut.GetById("any_book_id")
+
+	require.Equal(t, "any_book_id", getBookRepo.input)
+}

afa0bb2f3192e6347aa1495aeb90070ffd616120
KPMGE <jesuskevin254@gmail.com>
2022-04-29 10:08:52 -0300

    :recycle: move ListBooksController to its right folder

---
+++ src/presentation/controllers/list-books.go
@@ -0,0 +1,33 @@
+package controllers
+
+import (
+	"encoding/json"
+
+	usecases "github.com/KPMGE/go-users-clean-api/src/application/useCases"
+	"github.com/KPMGE/go-users-clean-api/src/presentation/helpers"
+	"github.com/KPMGE/go-users-clean-api/src/presentation/protocols"
+)
+
+type ListBooksController struct {
+	useCase *usecases.ListBooksUseCase
+}
+
+func NewListBooksController(useCase *usecases.ListBooksUseCase) *ListBooksController {
+	return &ListBooksController{
+		useCase: useCase,
+	}
+}
+
+func (controller *ListBooksController) Handle(request *protocols.HttpRequest) *protocols.HttpResponse {
+	books, err := controller.useCase.List()
+	if err != nil {
+		return helpers.ServerError(err)
+	}
+
+	jsonBooks, err := json.Marshal(books)
+	if err != nil {
+		return helpers.ServerError(err)
+	}
+
+	return helpers.Ok(jsonBooks)
+}

5eea9e77330a2e673da74bdec22504d9a2969565
KPMGE <jesuskevin254@gmail.com>
2022-07-31 16:45:03 -0300

    :sparkles: implement check user by name, email and id from postgres

--- src/infrasctructure/repositories/postgres-repository/postgres-users.go
+++ src/infrasctructure/repositories/postgres-repository/postgres-users.go
@@ -3,7 +3,6 @@ package postgresrepository
 import (
 	"database/sql"
 	"github.com/KPMGE/go-users-clean-api/src/domain/entities"
-	"log"
 )
 
 type PostgresUserRepository struct {
@@ -18,10 +17,44 @@ func (repo *PostgresUserRepository) Save(user *entities.User) error {
 }
 
 func (repo *PostgresUserRepository) CheckByEmail(email string) bool {
+	query := `SELECT email FROM users WHERE email = ($1)`
+	rows, err := repo.db.Query(query, email)
+	CheckError(err)
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var foundEmail string
+
+		err = rows.Scan(&foundEmail)
+		CheckError(err)
+
+		if foundEmail != "" {
+			return true
+		}
+	}
+
 	return false
 }
 
 func (repo *PostgresUserRepository) CheckByUserName(userName string) bool {
+	query := `SELECT user_name FROM users WHERE user_name = ($1)`
+	rows, err := repo.db.Query(query, userName)
+	CheckError(err)
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var foundUserName string
+
+		err = rows.Scan(&foundUserName)
+		CheckError(err)
+
+		if foundUserName != "" {
+			return true
+		}
+	}
+
 	return false
 }
 
@@ -32,20 +65,12 @@ func (repo *PostgresUserRepository) List() []*entities.User {
 	rows, err := repo.db.Query(query)
 
 	defer rows.Close()
-
-	if err != nil {
-		log.Fatal(err)
-	}
+	CheckError(err)
 
 	for rows.Next() {
 		var user entities.User
-
 		err := rows.Scan(&user.ID, &user.CreatedAt, &user.UpdatedAt, &user.Name, &user.UserName, &user.Email)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
+		CheckError(err)
 		users = append(users, &user)
 	}
 
@@ -57,6 +82,23 @@ func (repo *PostgresUserRepository) Delete(userId string) error {
 }
 
 func (repo *PostgresUserRepository) CheckById(userId string) bool {
+	query := `SELECT id FROM users WHERE id = ($1)`
+	rows, err := repo.db.Query(query, userId)
+	CheckError(err)
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var foundUserId string
+
+		err = rows.Scan(&foundUserId)
+		CheckError(err)
+
+		if foundUserId != "" {
+			return true
+		}
+	}
+
 	return false
 }
 

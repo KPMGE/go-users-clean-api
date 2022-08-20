b40b1c800f28aa99dfa7b0c12982568032a810b9
KPMGE <jesuskevin254@gmail.com>
2022-08-20 16:32:57 -0300

    :sparkles: add RequiredParameterValidator and ValidationComposite

---
+++ src/validation/validators/required-parameter.go
@@ -0,0 +1,38 @@
+package validators
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+)
+
+type RequiredParameterValidation struct {
+	parameterName string
+}
+
+func getAttr(obj interface{}, fieldName string) reflect.Value {
+	pointToStruct := reflect.ValueOf(obj) // addressable
+	curStruct := pointToStruct.Elem()
+	if curStruct.Kind() != reflect.Struct {
+		panic("not a struct")
+	}
+	curField := curStruct.FieldByName(fieldName) // type: reflect.Value
+	if !curField.IsValid() {
+		panic("not found:" + fieldName)
+	}
+	return curField
+}
+
+func (r *RequiredParameterValidation) Validate(input any) error {
+	val := getAttr(input, r.parameterName)
+	if val.IsZero() {
+		return errors.New(fmt.Sprintf("Missing parameter %s!", r.parameterName))
+	}
+	return nil
+}
+
+func NewRequiredParameterValidation(fieldName string) *RequiredParameterValidation {
+	return &RequiredParameterValidation{
+		parameterName: fieldName,
+	}
+}

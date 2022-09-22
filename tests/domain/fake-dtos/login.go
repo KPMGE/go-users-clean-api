c2e1fb107f3daca3760652a5cd32081e9a40b87b
KPMGE <jesuskevin254@gmail.com>
2022-09-22 07:49:54 -0300

    :white_check_mark: ensure LoginController returns right data on success

--- tests/domain/fake-dtos/login.go
+++ tests/domain/fake-dtos/login.go
@@ -4,6 +4,7 @@ import domaindto "github.com/KPMGE/go-users-clean-api/src/domain/domain-dto"
 
 func MakeFakeLoginInputDTO() *domaindto.LoginInputDTO {
 	return &domaindto.LoginInputDTO{
+		UserName: "any_username",
 		Email:    "any@email.com",
 		Password: "any password",
 	}
---
+++ tests/presentation/controllers/login_test.go
@@ -0,0 +1,70 @@
+package controllers_test
+
+import (
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	domaindto "github.com/KPMGE/go-users-clean-api/src/domain/domain-dto"
+	usecases "github.com/KPMGE/go-users-clean-api/src/domain/useCases"
+	"github.com/KPMGE/go-users-clean-api/src/presentation/helpers"
+	"github.com/KPMGE/go-users-clean-api/src/presentation/protocols"
+	fakedtos "github.com/KPMGE/go-users-clean-api/tests/domain/fake-dtos"
+	"github.com/stretchr/testify/require"
+)
+
+type LoginServiceMock struct {
+	Input  *domaindto.LoginInputDTO
+	Output string
+	Error  error
+}
+
+func (s *LoginServiceMock) Login(input *domaindto.LoginInputDTO) (string, error) {
+	s.Input = input
+	return s.Output, s.Error
+}
+
+func NewLoginServiceMock() *LoginServiceMock {
+	return &LoginServiceMock{
+		Input:  nil,
+		Output: "token",
+		Error:  nil,
+	}
+}
+
+type LoginController struct {
+	srv usecases.LoginUseCase
+}
+
+func (c *LoginController) Handle(request *protocols.HttpRequest) *protocols.HttpResponse {
+	var input domaindto.LoginInputDTO
+	json.Unmarshal(request.Body, &input)
+
+	token, _ := c.srv.Login(&input)
+
+	return helpers.Ok(token)
+}
+
+func NewLoginController(srv usecases.LoginUseCase) *LoginController {
+	return &LoginController{
+		srv: srv,
+	}
+}
+
+func FakeLoginRequest() *protocols.HttpRequest {
+	inputJson, err := json.Marshal(fakedtos.MakeFakeLoginInputDTO())
+	if err != nil {
+		panic("json Marshal fail at: FakeLoginRequest")
+	}
+	return protocols.NewHttpRequest(inputJson, nil)
+}
+
+func TestLoginController_ShouldReturnOkOnSuccess(t *testing.T) {
+	serviceMock := NewLoginServiceMock()
+	sut := NewLoginController(serviceMock)
+
+	httpRequest := sut.Handle(FakeLoginRequest())
+
+	require.Equal(t, http.StatusOK, httpRequest.StatusCode)
+	require.Equal(t, serviceMock.Output, httpRequest.Body)
+}
